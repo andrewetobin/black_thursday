05077f39c348634be5c3427c24fb6ef83650fcd7
andrewetobin <andrewetobin@gmail.com>
2018-07-25 06:11:34 -0600

    adds find by first/last name to customer repo

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -3,12 +3,26 @@ require_relative 'customer'
 
 class CustomerRepository
   include RepoHelper
+  attr_reader :repo
 
   def initialize(file_contents)
     @repo = file_contents.map do |customer|
       Customer.new(customer)
     end
   end
+
+  def find_all_by_first_name(name)
+    @repo.find_all do |customer|
+      customer.first_name.downcase.include?(name.downcase)
+    end
+  end
+
+  def find_all_by_last_name(name)
+    @repo.find_all do |customer|
+      customer.last_name.downcase.include?(name.downcase)
+    end
+  end
+
 
 
 
--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -19,6 +19,27 @@ class CustomerRepositoryTest < Minitest::Test
     assert_instance_of CustomerRepository, @customer_repo
   end
 
+  def test_all_will_return_entire_repo
+    assert_equal @customer_repo.repo, @customer_repo.all
+  end
+
+  def test_find_by_id_number
+    assert_equal @customer_repo.all[25], @customer_repo.find_by_id(26)
+    assert_nil @customer_repo.find_by_id(6521)
+  end
+
+  def test_it_can_find_all_by_first_name
+    assert_equal [@customer_repo.all[664]], @customer_repo.find_all_by_first_name('Octavia')
+    assert_equal [], @customer_repo.find_all_by_first_name('Tonoccus')
+    assert_equal 21, @customer_repo.find_all_by_first_name('Ed').length
+  end
+
+  def test_it_can_find_by_last_name
+    assert_equal [@customer_repo.all[798]], @customer_repo.find_all_by_last_name('Oberbrunner')
+    assert_equal [], @customer_repo.find_all_by_last_name('Tobin')
+    assert_equal 22, @customer_repo.find_all_by_last_name('son').length
+  end
+
 
 
 
