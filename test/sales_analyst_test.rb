ff239ae4ca98cf8d80f2e538c56469393756ac54
mmbensalah <mmbensalah@gmail.com>
2018-07-31 15:09:42 -0600

    adds test to sales analyst test

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -128,9 +128,10 @@ class SalesAnalystTest < Minitest::Test
   end
 
   def test_top_revenue_earners
-    # assert_equal 10, @sales_analyst.top_revenue_earners(10).count
-    # assert_instance_of Merchant, @sales_analyst.top_revenue_earners(10)[0]
-    assert_equal [], @sales_analyst.revenue_by_merchant_each
+    assert_equal 10, @sales_analyst.top_revenue_earners(10).count
+    assert_equal 20, @sales_analyst.top_revenue_earners.count
+    assert_instance_of Merchant, @sales_analyst.top_revenue_earners(10)[0]
+    assert_equal 12334634, @sales_analyst.top_revenue_earners(10)[0].id
   end
 
   def test_revenue_by_merchant
